c16580f0bffc1b4e5155b403d19ea2e97a37a288
selby <fetz.selby@gmail.com>
2018-12-07 19:05:32 +0000

    truncate limit added

--- src/config.js
+++ src/config.js
@@ -3,3 +3,4 @@ export const REQUEST_DELAY = 1000;
 export const MAP_KEYS = 'AIzaSyC53deGuD9TB4tJsbB2h_NtOMzUxANOVZs';
 export const DEFAULT_MAP_CENTER = [51.165691, 10.451526000000058];
 export const DEFAULT_MAP_ZOOM = 5;
+export const ADDRESS_TRUNCATE_LIMIT = 45;
