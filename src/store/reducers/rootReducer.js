391bf389446dba9a9e8d0efc3c1ee98e3296a4e7
selby <fetz.selby@gmail.com>
2018-12-09 09:19:58 +0000

    geo reducer added

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import locationReducer from './locationReducer';
 import appReducer from './appReducer';
+import geoReducer from './geoReducer';
 
 export default combineReducers({
     locations : locationReducer,
-    app : appReducer
+    app : appReducer,
+    geo: geoReducer
 })
