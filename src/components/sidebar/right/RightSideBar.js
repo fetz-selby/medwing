32a224052b6f9044ac6c02a83ffc0296a5d8533c
selby <fetz.selby@gmail.com>
2018-12-10 08:47:29 +0000

    logo removed

--- src/components/sidebar/right/RightSideBar.js
+++ src/components/sidebar/right/RightSideBar.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../../../assets/styles/layout.css';
-import logo from '../../../assets/icons/medwing.svg';
 import './right-sidebar.css';
 import DetailForm from '../../detail-form';
 
-const SideBarLogo = () =>{
+// const SideBarLogo = () =>{
 
-    const logo_label = 'medwing';
-    return <ul className='logo-container'>
-                <li className='logo'>
-                     <img src={logo} alt={logo_label}></img>
-                </li>
-            </ul>
-}
+//     const logo_label = 'medwing';
+//     return <ul className='logo-container'>
+//                 <li className='logo'>
+//                      <img src={logo} alt={logo_label}></img>
+//                 </li>
+//             </ul>
+// }
 
 
 const SideBarMenuItemContainer = props =>
         <nav className={props.showSideBar ? 'show sidebar-container sidebar-right' : 'sidebar-container sidebar-right'}>
-            <SideBarLogo />
             <DetailForm suggestions={props.addressSuggestions}
                         location={props.location} 
                         isUpdate={props.isUpdate}
