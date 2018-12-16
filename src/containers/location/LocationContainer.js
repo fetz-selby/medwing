85f091f5b65ac142de4cee2c6b934cf7c1e3dafb
selby <fetz.selby@gmail.com>
2018-12-16 15:07:15 +0000

    impl on click to show right sidebar

--- src/containers/location/LocationContainer.js
+++ src/containers/location/LocationContainer.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import * as locationActions from '../../store/actions/locations/locationActionCreators';
+import * as appActions from '../../store/actions/app/appActionCreators';
+
 
 import GoogleMap from '../../components/map';
 import './location.css';
 
 class LocationContainer extends Component {
 
-    componentDidMount = () => {
-       
+    onLocationMarkerClickedHandler=(id)=>{
+        console.log('id => '+id);
+        this.props.markerSelected(id);
+        this.props.isLocationUpdate();
+        this.props.showRightSideBar();
     }
 
     render() {
         const {locations} = this.props;
         return <div className='map-container'>
-                <GoogleMap locations={locations}/>
+                <GoogleMap onLocationMarkerClicked={this.onLocationMarkerClickedHandler} 
+                           locations={locations}/>
             </div>
     }
 }
@@ -29,7 +35,10 @@ const mapStateToProps = state =>{
 const mapDispatchToProps = dispatch =>{
     return {
         loadLocations : () => dispatch(locationActions.fetchLocations()),
-        searchLocation : (val) => dispatch(locationActions.searchLocation(val)) 
+        searchLocation : (val) => dispatch(locationActions.searchLocation(val)),
+        markerSelected : (id)=>dispatch(locationActions.widgetSelectedLocation(id)),
+        showRightSideBar:() => dispatch(appActions.showRightSideBar()),
+        isLocationUpdate:()=>dispatch(appActions.isLocationUpdate()),
     }
 }
 
