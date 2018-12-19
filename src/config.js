c20ccd22cdda34a4aec5002860a7c614ba3cdae1
selby <fetz.selby@gmail.com>
2018-12-19 17:36:21 +0000

    delay increased

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const BASE_URL = 'http://localhost:8001';
-export const REQUEST_DELAY = 1000;
+export const REQUEST_DELAY = 1500;
 export const MAP_KEYS = 'AIzaSyC53deGuD9TB4tJsbB2h_NtOMzUxANOVZs';
 export const DEFAULT_MAP_CENTER = [51.165691, 10.451526000000058];
 export const DEFAULT_MAP_ZOOM = 5;
