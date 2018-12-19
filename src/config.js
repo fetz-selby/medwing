a6164a8eec0177a9e36d3892054fdd9240f346dd
selby <fetz.selby@gmail.com>
2018-12-19 22:10:17 +0000

    google key

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const BASE_URL = 'http://localhost:8001';
 export const REQUEST_DELAY = 1500;
-export const MAP_KEYS = 'AIzaSyC53deGuD9TB4tJsbB2h_NtOMzUxANOVZs';
+export const MAP_KEYS = ''; /* insert google key */
 export const DEFAULT_MAP_CENTER = [51.165691, 10.451526000000058];
 export const DEFAULT_MAP_ZOOM = 5;
 export const ADDRESS_TRUNCATE_LIMIT = 45;
